adf92c33c381c495dfdab49e1e56b6c20fd11887
kimberlycasamina <kcasamina@tuta.io>
2023-06-27 21:22:56 +0800

    feat: added navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart, ShoppingCartSimple } from '@phosphor-icons/react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} color="white"/>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
